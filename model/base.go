369af633ddf914c7e62f08a7fec23bda2379fd76
tudou <tudou@tudou.com>
2022-11-18 14:35:00 +0800

    ini

--- model/base.go
+++ model/base.go
@@ -157,7 +157,7 @@ func (m *BaseModel) GetByPage(pagination PaginationQuery) utils.PageData {
 		DataList: mapRes,
 	}
 }
-func (m *BaseModel) SetMod() error {
+func (m *BaseModel) SetMode() error {
 	sql := "set sql_mode ='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';"
 	return m.ExecuteSql(sql)
 }
