0adaef5c346823a704a3b33756aa6e66a1a258cd
tudou <tudou@tudou.com>
2022-11-20 02:23:19 +0800

    ini

---
+++ config/system.go
@@ -0,0 +1,14 @@
+/**
+ * Created by PhpStorm.
+ * @file   system.go
+ * @author 李锦 <Lijin@cavemanstudio.net>
+ * @date   2022/11/20 02:10
+ * @desc   system.go
+ */
+
+package config
+
+// System 系统配置
+type System struct {
+	Mode string `yaml:"mode"`
+}
